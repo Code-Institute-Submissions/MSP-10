6cee1081fca7afcf6bc75a940d922542c86b72dd
Sphere42 <Sphere42@gmail.com>
2021-12-23 14:05:39 +0000

    Create Policies.html and views for page
    Add the view and urls for the policies page. Update the template layout with placeholder test boxes etc.

--- mochabean/views.py
+++ mochabean/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     # View for displaying the landing page
     return render(request, 'index.html')
+
+def policies(request):
+    # View for displaying the resource list page
+    return render(request, 'information/policies.html')
