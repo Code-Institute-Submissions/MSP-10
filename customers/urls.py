889546d3b92e2288619e282ffa83e81dbde9d267
Sphere42 <Sphere42@gmail.com>
2022-01-08 21:18:02 +0000

    Add password reset default templates
    Adding in the django default password templates. This is due to reset functionality being nested in the customers app

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse_lazy
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'customers'
@@ -10,8 +12,31 @@ urlpatterns = [
     path('customers/register/', views.registerPage, name='register-page'),
     path('customers/logout/', views.logoutPage, name='logout-page'),
     
-    path('customers/reset_password', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('customers/reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('customers/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('customers/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
+    
+    path('customers/reset_password/', 
+        auth_views.PasswordResetView.as_view(
+            template_name='password/password_reset_form.html',
+            success_url=reverse_lazy('customers:password_reset_done'),
+            email_template_name='password/password_reset_email.html'), 
+            name="reset_password"
+    ),
+    path('customers/reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='password/password_reset_done.html'
+        ),
+        name="password_reset_done"),
+    path(
+        'password_reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password/password_reset_confirm.html',
+            success_url=reverse_lazy('customers:password_reset_complete')
+        ),
+        name='password_reset_confirm'
+    ),
+    path('customers/reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password/password_reset_complete.html'),
+        name="password_reset_complete"
+    ),
 ]
--- mochabean/settings.py
+++ mochabean/settings.py
@@ -138,4 +138,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = Customer
+
+# Simple Mail Transfer Protocol (SMTP) Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
