736427308db3149cf1544d841c9652422ce58532
Sphere42 <Sphere42@gmail.com>
2022-01-08 18:00:19 +0000

    Add inital password reset urls
    Adding the default django urls for password reset ability

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'customers'
@@ -10,4 +9,9 @@ urlpatterns = [
     path('customers/login/', views.loginPage, name='login-page'),
     path('customers/register/', views.registerPage, name='register-page'),
     path('customers/logout/', views.logoutPage, name='logout-page'),
+    
+    path('customers/reset_password', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('customers/reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('customers/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('customers/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
