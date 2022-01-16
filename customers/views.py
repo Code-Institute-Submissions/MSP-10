c9d7483a8e2b6ff23eb641df36c688ad3caa6496
Sphere42 <Sphere42@gmail.com>
2022-01-16 13:48:54 +0000

    Update profile page view
    Updated the profile page view to redirect the customer to the update page if they do not have address details in the DB

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,6 @@
+from asyncio.windows_events import NULL
+from logging import NullHandler
+from queue import Empty
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, UpdateProfileForm
 from django.contrib.auth import authenticate, login, logout
@@ -51,8 +54,16 @@ def customer_profile_view(request, pk):
     customer = User.objects.get(id=pk)
     current_user = request.user
     profile = Profile.objects.get(name=current_user)
-    context = {'customer':customer, 'profile':profile}
-    return render(request, 'profile_view.html', context)
+    
+    # The below will direct the customer to the update page if address is empty
+    addy = Profile.objects.get(name=current_user).address1
+    print(str(addy))
+    if addy == None:
+        return redirect('customers:customer-update', pk)
+    # Loads the customers Profile Page
+    else:
+        context = {'customer':customer, 'profile':profile}
+        return render(request, 'profile_view.html', context)
 
 def customer_profile_update(request, pk):
     # Ensuring only staff can view amend page
@@ -70,8 +81,8 @@ def customer_profile_update(request, pk):
                 form.save()
                 messages.success(request, 'Provider Updated Successfully')
                 form = UpdateProfileForm()
-                return redirect('customers:customer-profile')
+                return redirect('customers:customer-profile', pk)
         else:
             context = {'form': form}
-            return render(request, 'customers:profile_update.html', context)
+            return render(request, 'profile_update.html', context)
         
