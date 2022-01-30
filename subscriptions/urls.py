1ec19ff9f7b8d4c998527d46d52d958ebbc57154
Sphere42 <Sphere42@gmail.com>
2022-01-30 14:39:20 +0000

    Add urls for payment

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -10,4 +10,8 @@ app_name = 'subscriptions'
 urlpatterns = [
     path('unsubscribe/', views.newsletter_subscription_delete, name='newsletter-unsubscribe'),
     path('unsubscribed/', views.newsletter_subscription_confirmation, name='newsletter-unsubscribed'),
+    
+    path('product/checkout/', views.subscription_checkout, name='subscription-checkout'),
+    path('product/success/', views.subscription_success, name='subscription-success'),
+    path('product/cancel/', views.newsletter_subscription_confirmation, name='subscription-cancel'),
 ]
